4e93766c2b38691e374d4ea94774d8eefa07c4ef
scene <bergeqhmk09@gmail.com>
2021-11-01 15:42:15 +0800

    Initial commit

---
+++ TabLayout/src/main/java/wiki/scene/tab/listener/CustomTabEntity.java
@@ -0,0 +1,34 @@
+package wiki.scene.tab.listener;
+
+import androidx.annotation.DrawableRes;
+
+/**
+ * @Author: AriesHoo on 2018/12/3 13:07
+ * @E-Mail: AriesHoo@126.com
+ * @Function: {@link wiki.scene.tab.CommonTabLayout} 属性设置
+ * @Description:
+ */
+public interface CustomTabEntity {
+    /**
+     * tab文本
+     *
+     * @return
+     */
+    String getTabTitle();
+
+    /**
+     * tab 选中icon 资源id
+     *
+     * @return
+     */
+    @DrawableRes
+    int getTabSelectedIcon();
+
+    /**
+     * tab未选中icon资源
+     *
+     * @return
+     */
+    @DrawableRes
+    int getTabUnselectedIcon();
+}
---
+++ TabLayout/src/main/java/wiki/scene/tab/listener/ITabLayout.java
@@ -0,0 +1,15 @@
+package wiki.scene.tab.listener;
+
+/**
+ * @Author: AriesHoo on 2018/11/30 16:32
+ * @E-Mail: AriesHoo@126.com
+ * @Function:
+ * @Description:
+ */
+public interface ITabLayout {
+
+    /**
+     * 更新Tab样式
+     */
+    void updateTabStyles();
+}
